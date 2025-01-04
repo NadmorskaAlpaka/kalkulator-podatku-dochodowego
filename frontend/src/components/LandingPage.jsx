fa8e4a6f2cdfcdfdcf730073a055ebe7d6235035
NadmorskaAlpaka <grzesio26500@gmail.com>
2025-01-04 13:14:00 +0100

    class to className fix

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -7,12 +7,12 @@ const LandingPage = () => {
         <section id="landing-page">
             <div className="landig-page__box">
                 <div className="landig-page__content">
-                    <h1 className="title">Kalkulator podatku dochodowego<br class="landing-page__line-break"/> dla osób fizycznych i firm</h1>
+                    <h1 className="title">Kalkulator podatku dochodowego<br className="landing-page__line-break"/> dla osób fizycznych i firm</h1>
                     <h2 className="sub-title">Oblicz podatek dochodowy w prosty sposób</h2>
                     <p className="text">Sprawdź ile wyniesie twój podatek</p>
                     <button className="cta">Zaczynamy</button>
                 </div>
-                <img src={Image1} class="landing-page__img" alt="person with computer ilustration" />
+                <img src={Image1} className="landing-page__img" alt="person with computer ilustration" />
             </div>
         </section>
     )
