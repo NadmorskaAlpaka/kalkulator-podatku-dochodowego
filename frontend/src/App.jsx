1d399a92b807f68c74fdb84645102c9ae8409752
NadmorskaAlpaka <grzesio26500@gmail.com>
2025-03-28 23:01:36 +0100

    populating tax input in settings component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { defaultValues } from "./defaultTaxValues";
 import "./styles/app.css";
 import Home from "./pages/Home";
 import NoPage from "./components/NoPage";
@@ -15,7 +17,6 @@ import ScrollToTop from "./components/ScrollToTop";
 import Tax from "./pages/Tax";
 import Settings from "./pages/Settings";
 import TaxCompare from "./pages/TaxCompare";
-import { useState, useEffect } from "react";
 
 const LOCAL_STORAGE_KEY = "taxParameters";
 
@@ -23,79 +24,18 @@ function App() {
 
   const [taxParameters, setTaxParameters] = useState(() => {
     const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    return savedData ? JSON.parse(savedData) : 
-    {
-      socialContributions: {
-        minSocialContributionBasis: 5203.80,
-        uEmerytalnePercentage: 19.52,
-        uRentowePercentage: 8,
-        uChorobowePercentage: 2.45,
-        uWypadkowePercentage: 1.67,
-        funduszPracyPercentage: 2.45,
-      },
-      healthCountributions: {
-        mimIncome: 4666,
-        avgIncomeLastQuaterPrevYear: 8549.18,
-        taxScale: {
-          healthCountributionsBasisPercentage: 75,
-          valuePercentage: 9,
-          minHealthCountributions: 314.96
-        },
-        flatTax: {
-          healthCountributionsBasisPercentage: 75,
-          valuePercentage: 4.9,
-          minHealthCountributions: 314.96
-        },
-        lumpSumTax: {
-          small:{
-            maxIncome: 60000,
-            healthCountributionsBasisPercentage: 60,
-            valuePercentage: 9,
-          },
-          medium: {
-            maxIncome: 300000,
-            healthCountributionsBasisPercentage: 100,
-            valuePercentage: 9,
-          },
-          big: {
-            minIncome: 300000,
-            healthCountributionsBasisPercentage: 180,
-            valuePercentage: 9,
-          }
-        },
-      },
-      taxBreaks: {
-        internet: {
-          maxValue: 760
-        },
-        children: {
-          one: {
-            value: 1112.04,
-            limit: 56000,
-            spousLimit: 112000
-          },
-          two: 2224.08,
-          three: 4224.12,
-          four: 6924.12,
-        },
-        bloodDonation: {
-          maxIncomPercentage: 6,
-          value: 130
-        },
-        newTechnology: 50
-      },
-      danina: {
-        valuePercentage: 4,
-        minIncome: 1000000
-      },
-      taxFreeAmout: 30000
-    }
+    return savedData ? JSON.parse(savedData) : defaultValues; 
   });
 
   useEffect(() => {
     console.log("TaxParameters",taxParameters);
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(taxParameters));
   }, [taxParameters]);
+
+  const reset = () => {
+    setTaxParameters(defaultValues);
+    console.log(taxParameters)
+  }
 
   return (
     <>
@@ -105,7 +45,7 @@ function App() {
         <Navigation />
           <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/ustawienia" element={<Settings taxParameters={taxParameters} />} />
+            <Route path="/ustawienia" element={<Settings taxParameters={taxParameters} reset={reset}/>} />
             {/* <Route path="/logowanie" element={<Login />}/>
             <Route path="/rejestracja" element={<Register />} />
             <Route path="/pracownik" element={""} /> */}
